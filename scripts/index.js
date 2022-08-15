fb88fbb47e53c240d4707e04d62d441a5b69be3c
sergejjlozjuk <50342457+sergejjlozjuk@users.noreply.github.com>
2022-08-15 15:22:14 +0300

    'second review'

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,27 @@ let changeButton = document.querySelector('.user__change-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
 let form = document.querySelector('.form');
-let formName = document.querySelector('.form__name');
-let formInfo = document.querySelector('.form__info');
+let formName = document.querySelector('.form__input_name');
+let formInfo = document.querySelector('.form__input_info');
 let userName = document.querySelector('.user__name');
 let userInfo = document.querySelector('.user__info');
 
-function change_name () {
+function changeName () {
  popup.classList.add('popup_active');
- formName.value = userName.innerText;
- formInfo.value = userInfo.innerText;
+ formName.value = userName.textContent;
+ formInfo.value = userInfo.textContent;
 }
-changeButton.addEventListener('click', change_name);
+changeButton.addEventListener('click', changeName);
 
-function close_popup () {
+function closePopup () {
     popup.classList.remove('popup_active');
 }
-closeButton.addEventListener('click', close_popup);
+closeButton.addEventListener('click', closePopup);
 
-function save_changes (evt) {
+function saveChanges (evt) {
     evt.preventDefault();
     userName.textContent = formName.value;
     userInfo.textContent = formInfo.value;
-    close_popup();
+    closePopup();
 }
-form.addEventListener('submit', save_changes);
+form.addEventListener('submit', saveChanges);
